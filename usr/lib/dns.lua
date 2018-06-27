d93dd53809b64d168eb18e4837559a5e1a00677d
Jannick <jannick.van.eeghem@gmail.com>
2018-06-27 14:47:46 +0200

    Added the hosting system. Done.

--- usr/lib/dns.lua
+++ usr/lib/dns.lua
@@ -12,15 +12,44 @@ end
 function dns.lookup(hostname)
 checkarg(1, hostname, "string")
 local server
-while not server do
+repeat
 server = dns.get()
-end
+until server
 comp.modem.open(53)
-comp.modem.send(server, "dns lookup", hostname)
+comp.modem.send(server, 53, "dns lookup", hostname)
 local _, _, sender, _, addr = event.pull("modem_message")
-while sender ~= server do
-comp.modem.send(server, "dns lookup", hostname)
-local _, _, sender, _, addr = event.pull("modem_message")
-end
 comp.modem.close(53)
+end
+function dns.register(hostname)
+local server
+repeat
+server = dns.get()
+until server
+local modem = comp.modem
+modem.send(server, 53, "dns register", modem.address, hostname)
+end
+function dns.new()
+comp.modem.open(53)
+while true do
+local event, _, sender, _, func, arg1, arg2 = event.pullMultiple("modem_message", interrupt)
+if event == "modem_message" then
+if func == "dns ping" then
+comp.modem.send(sender, 53, "dns pong")
+elseif func == "dns lookup" then
+local file = io.open("/etc/hosts", "r")
+local domains = serialization.unserialize(file.read("*a"))
+comp.modem.send(sender, 53, "dns lookup", domains[hostname])
+elseif func == "dns register" then
+local file = io.open("/etc/hosts", "r")
+local domains = serialization.unserialize(file.read("*a"))
+domains[arg2] = arg1
+file = nil
+local file = io.open("/etc/hosts", "w")
+file.write(serialization.serialize(domains))
+end
+else
+print("Closing dns...")
+return
+end
+end
 end
